1d8560eafff5f77bd0af89ab8840ebd8415be17f
Vaidas Pilkauskas <vaidasp@wix.com>
2017-11-01 17:10:55 +0200

    Remove logging of base host and port

--- test-lib/http-testkit.js
+++ test-lib/http-testkit.js
@@ -1,7 +1,4 @@
 module.exports = ({host = "http://localhost", port}) => {
-
-  console.log(host, port)
-
   return {
     http: require('../test-lib/http')(`${host}:${port}`),
     matchers: require('../test-lib/response-matchers')
